16896e08c57f5ebb344b3857905e5b2daa3d3e05
rub9yz <rubenyz143@gmail.com>
2020-01-11 21:31:57 +0100

    created files

---
+++ eindopdracht/xmega/main.c
@@ -0,0 +1,19 @@
+/*
+ * xmega.c
+ *
+ * Created: 09-01-2020 17:29:17
+ * Author : Ruben
+ */ 
+
+#define F_CPU 32000000UL
+
+#include <avr/io.h>
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
